1802116f80724ae21760127a066a3c5562ed15de
Rhapsodistic <jonathanxqs@gmail.com>
2016-06-23 00:35:35 +0800

    Solve No.13 by python

---
+++ 13.py
@@ -0,0 +1,9 @@
+class Solution:
+    def strStr(self, source, target):
+        # write your code here
+        if None==source or target==None:
+        	return -1
+
+        s1= source.find(target)
+        return s1
+
