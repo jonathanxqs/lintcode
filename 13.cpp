f504b62af72ed05df5c3cd576e975d152d0840fd
Rhapsodistic <jonathanxqs@gmail.com>
2016-06-26 20:15:25 +0800

    Dealing with No.13 in C++

---
+++ 13.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    /**
+     * Returns a index to the first occurrence of target in source,
+     * or -1  if target is not part of source.
+     * @param source string to be scanned.
+     * @param target string containing the sequence of characters to match.
+     */
+    int strStr(const char *source, const char *target) {
+        // write your code here
+        
+
+
+        return -1;
+    }
+};
