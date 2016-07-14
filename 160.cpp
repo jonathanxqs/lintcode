289fd5d2061565811359786d8df53fe223df0a55
Rhapsodistic <jonathanxqs@gmail.com>
2016-07-14 23:15:39 +0800

    Solve No.179 in C++ bit

---
+++ 160.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    /**
+     * @param num: the rotated sorted array
+     * @return: the minimum number in the array
+     */
+    int findMin(vector<int> &num) {
+        // write your code here
+        int min1=MAX_INT,l=0,r=num.size()-1,mid=0;
+        while (l<=r){
+        	mid=(l+r)/2;
+        	min1=min(min1,num[mid]);
+        	if (num[l]<num[r]){
+        		
+        	}
+
+        }
+    }
+};
