09814a5b91a17f6896a6c3d6999f1ec7c13123b3
Rhapsodistic <jonathanxqs@gmail.com>
2016-07-23 17:07:22 +0800

    140 C++ AC,Python Wa

--- 140.cpp
+++ 140.cpp
@@ -5,24 +5,17 @@ public:
      * @return: An integer
      */
     int fastPower(int a, int b, int n) {
-        // write your code here
-        int md_a=a%b,i=0,j;
-        long long da=1;
-        std::vector<long long> xl(b+2);
-
-        xl[0]=(da%b);
-
-        while (i<b){
-
-        	da=(da*md_a)%b;
-        	i++;   
-        	if (find(xl.begin(),xl.begin()+i-1,da) != xl.begin()+i) break;
-        	xl[i]=(da);
-        		
-
+        if (n == 0) {
+            return 1 % b;
         }
-
-        return xl[n%i];
-
+        if (n == 1) {
+            return a % b;
+        }
+        long long temp = fastPower(a, b, n / 2);
+        if (n % 2 == 0) {
+            return (temp * temp) % b;
+        } else {
+            return ((temp * temp) % b * a) % b;
+        }
     }
 };
---
+++ 140.py
@@ -0,0 +1,17 @@
+
+
+class Solution:
+    """
+    @param a, b, n: 32bit integers
+    @return: An integer
+    """
+    def fastPower(self, a, b, n):
+        ans = 1
+        while b > 0:
+            if b % 2==1:
+                ans = ans * a % n
+            a = a * a % n
+            b = b / 2
+        return ans % n
+
+        # WA
