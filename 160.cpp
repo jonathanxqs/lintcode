dbca03febf6753b70bf38a0c6c795a6ff61f3a87
Rhapsodistic <jonathanxqs@gmail.com>
2016-07-27 23:37:16 +0800

    Solve No.160  Worst O(n)   fake binary search

--- 160.cpp
+++ 160.cpp
@@ -6,15 +6,34 @@ public:
      */
     int findMin(vector<int> &num) {
         // write your code here
-        int min1=MAX_INT,l=0,r=num.size()-1,mid=0;
-        while (l<=r){
-        	mid=(l+r)/2;
-        	min1=min(min1,num[mid]);
-        	if (num[l]<num[r]){
-        		r=mid-1;
-        		
-        	}
-
+         int lo = 0;
+        int hi = num.size()-1;
+        while(lo<hi){
+            int mid = lo + (hi - lo)/2;
+            if(num[mid]>num[hi]){
+                lo = mid+1;
+            }else if(num[mid]<num[hi]){
+                hi = mid;
+            }else{
+                hi--;
+            }
         }
+        return num[lo];
+        
+       /* 
+        int st = 0;
+        int ed = num.size()-1;
+        while(st<ed){
+            int mid = st+(ed-st)/2;
+            if(num[mid] > num[ed]){
+                st = mid+1;
+            }else if(mid[mid] < num[ed]){
+                ed = mid;
+            }else {
+                ed--;
+            }
+        }
+        return num[st];
+        */
     }
 };
