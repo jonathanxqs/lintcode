41513bcbbf9fb0888aeea016390b8cec5db7b047
Rhapsodistic <jonathanxqs@gmail.com>
2016-07-28 23:21:53 +0800

    Begin No.180

---
+++ 180.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    /**
+     *@param n: Given a decimal number that is passed in as a string
+     *@return: A string
+     */
+    string binaryRepresentation(string n) {
+        // wirte your code here
+        string i,j=n;
+        
+    }
+};
