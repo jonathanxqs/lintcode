dbc1d5cb66d8ed13d3b6a470af3a0cf5f4c06492
Rhapsodistic <jonathanxqs@163.com>
2016-08-05 00:15:53 +0800

    Solve No.117

--- 117.cpp
+++ 117.cpp
@@ -7,10 +7,15 @@ public:
     int jump(vector<int> A) {
         // wirte your code here
         int n=A.size(),f[n+10],j,i,k;
-        for (i=0;i<n+1;i++) f[i]=0;
-        f[0]=1;
+        for (i=0;i<n+1;i++) f[i]=INT_MAX/2;
+        f[0]=0;
     	for (i=0;i<n;i++){
-    		
+    		for (j=A[i];j>=1;j--){
+    			if (i+j>=n) continue;
+    			f[i+j]=min(f[i]+1,f[i+j]);
+    		}
     	}
+
+    	return f[n-1];
     }
 };
