49cc776efc8480dccb1bc0bd18a473a799bbf7d2
Rhapsodistic <jonathanxqs@163.com>
2016-08-04 23:58:25 +0800

    Solving No.117

---
+++ 117.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    /**
+     * @param A: A list of lists of integers
+     * @return: An integer
+     */
+    int jump(vector<int> A) {
+        // wirte your code here
+        int n=A.size(),f[n+10],j,i,k;
+        for (i=0;i<n+1;i++) f[i]=0;
+        f[0]=1;
+    	for (i=0;i<n;i++){
+    		
+    	}
+    }
+};
