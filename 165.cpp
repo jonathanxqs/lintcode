51fc9ff4eb12379b93f65b5fe3b4fc5b323ee054
Rhapsodistic <jonathanxqs@163.com>
2016-08-05 22:46:15 +0800

    Start to solve 165

---
+++ 165.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param ListNode l1 is the head of the linked list
+     * @param ListNode l2 is the head of the linked list
+     * @return: ListNode head of linked list
+     */
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        // write your code here
+        ListNode *p=l1,*q=l2;
+        while (p->next and q->next){
+        	
+        }
+    }
+};
