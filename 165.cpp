38c55eceb304df83af7a7424697b4560dc1fdb65
Rhapsodistic <jonathanxqs@163.com>
2016-08-06 15:07:50 +0800

    Solve No.165

--- 165.cpp
+++ 165.cpp
@@ -19,9 +19,37 @@ public:
      */
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
         // write your code here
-        ListNode *p=l1,*q=l2;
-        while (p->next and q->next){
-        	
+        ListNode *p=l1,*q=l2,*ml_h=new ListNode(),*ml_i=ml_h;
+
+        // cout<<"OK in initiation"<<endl;
+
+        while ( p!=nullptr or q!=NULL){
+
+        	// cout<<"OK in while loop"<<endl;
+
+
+        	if (q==nullptr or (p!=NULL && p->val < q->val) ){
+        		ml_i->val=p->val;
+        		// cout<<ml_i->val<<" ";
+        		p=p->next;   		
+
+        	}
+        	else{
+        		ml_i->val=q->val;
+        		// cout<<ml_i->val<<" ";
+        		q=q->next;
+        	}
+
+        	ListNode *ml_n=new ListNode();
+        	if (p==nullptr and q==NULL) break;
+        		
+        		ml_i->next=ml_n;
+        		ml_i=ml_n;
+
+
         }
+
+        return ml_h;
+
     }
 };
