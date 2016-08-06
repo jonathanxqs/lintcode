89843fa7c199d3f47cc261316b3742ec1c0951e2
Rhapsodistic <jonathanxqs@163.com>
2016-08-06 15:31:19 +0800

    Solve No.112

---
+++ 112.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *res = new ListNode(0);
+        res->next = head;
+        ListNode *tmp = res;
+        for (int i = 0; i < n; i++) {
+            head = head->next;
+        }
+        while (head != NULL) {
+            head = head->next;
+            tmp = tmp->next;
+        }
+        tmp->next = tmp->next->next;
+        return res->next;
+    }
+};
+
+
