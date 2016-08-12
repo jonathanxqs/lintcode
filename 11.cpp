d3c20e8e0132c85976bea6a86fbdbaf162dd3b5b
Rhapsodistic <jonathanxqs@163.com>
2016-08-12 22:47:21 +0800

    Start to handle No.11 69

---
+++ 11.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+ 
+ 
+class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Level order a list of lists of integer
+     */
+public:
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        // write your code here
+    }
+};
