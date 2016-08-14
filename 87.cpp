c59569b326441def96d6e035f94616740bfe933d
Rhapsodistic <jonathanxqs@163.com>
2016-08-14 21:47:51 +0800

    Solve No.87

--- 87.cpp
+++ 87.cpp
@@ -10,86 +10,57 @@
  *     }
  * }
  */
-
 class Solution {
 public:
-    string serialize(TreeNode *root) {
-        vector<TreeNode *> Q;
-        Q.push_back(root);
-
-        for (int i = 0; i < Q.size(); i++) {
-            TreeNode *node = Q[i];
-            if (node == NULL) {
-                continue;
-            }
-            Q.push_back(node->left);
-            Q.push_back(node->right);
-        }
-
-        while (Q.size() > 0 && Q[Q.size() - 1] == NULL) {
-            Q.pop_back();
-        }
-
-        if (Q.size() == 0) {
-            return "{}";
-        }
-
-        stringstream ss;
-        ss << "{" << Q[0]->val;
-        for (int i = 1; i < Q.size(); i++) {
-            if (Q[i] == NULL) {
-                ss << ",#";
-            } else {
-                ss << "," << Q[i]->val;
-            }
-        }
-        ss << "}";
-    
-        return ss.str(); 
-    }
-
-    TreeNode *deserialize(string data) {
-        if (data == "{}") {
+    /**
+     * @param root: The root of the binary search tree.
+     * @param value: Remove the node with given value.
+     * @return: The root of the binary search tree after removal.
+     */
+    TreeNode* removeNode(TreeNode* root, int value) {
+        // write your code here
+        if (root == NULL)
             return NULL;
+        TreeNode * head = new TreeNode();
+        head->left = root;
+        TreeNode * tmp = root, *father = head;
+
+        while (tmp != NULL) {
+            if (tmp->val == value)
+                break;
+            father = tmp;
+            if (tmp->val > value)
+                tmp = tmp->left;
+            else
+                tmp = tmp->right;
         }
+        if (tmp == NULL)
+            return head->left;
 
-        vector<string> vals = split(data.substr(1, data.size() - 2), ",");
-        TreeNode *root = new TreeNode(atoi(vals[0].c_str()));
-        queue<TreeNode *> Q;
-        Q.push(root);
+        if (tmp->right == NULL) {
+            if (father->left == tmp)
+                father->left = tmp->left;
+            else
+                father->right = tmp->left;
+        } else 
+        if (tmp->right->left == NULL) {
+            if (father->left == tmp)
+                father->left = tmp->right;
+            else
+                father->right = tmp->right;
 
-        bool isLeftChild= true;
-        for (int i = 1; i < vals.size(); i++) {
-            if (vals[i] != "#") {
-                TreeNode *node = new TreeNode(atoi(vals[i].c_str()));
-                if (isLeftChild) {
-                    Q.front()->left = node;
-                } else {
-                    Q.front()->right = node;
-                }
-                Q.push(node);
+            tmp->right->left = tmp->left;
+            
+        } else {
+            father = tmp->right;
+            TreeNode * cur = tmp->right->left;
+            while (cur->left != NULL) {
+                father = cur;
+                cur = cur->left;
             }
-            if (!isLeftChild) {
-                Q.pop();
-            }
-
-            isLeftChild = !isLeftChild; 
+            tmp->val = cur->val;
+            father->left = cur->right;
         }
-        return root;
-    }
-
-    vector<string> split(const string &str, string delim) {
-        vector<string> results;
-        int lastIndex = 0, index;
-        while ((index = str.find(delim, lastIndex)) != string::npos) {
-            results.push_back(str.substr(lastIndex, index - lastIndex));
-            lastIndex = index + delim.length();
-        }
-
-        if (lastIndex != str.length()) {
-            results.push_back(str.substr(lastIndex, str.length() - lastIndex));
-        }
-
-        return results;
+        return head->left;
     }
 };
