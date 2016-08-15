c18e7366f3c0706e292441ed8999cbad8fd15f68
Rhapsodistic <jonathanxqs@163.com>
2016-08-15 13:13:30 +0800

    Solve No.155

--- 155.cpp
+++ 155.cpp
@@ -16,7 +16,20 @@ public:
      * @param root: The root of binary tree.
      * @return: An integer
      */
+
     int minDepth(TreeNode *root) {
         // write your code here
+        if (root==NULL) return 0;
+        int min_tmp=INT_MAX;
+
+        if (root->left==NULL and root->right==NULL) return 1;
+
+
+        if (root->left) min_tmp=min(minDepth(root->left),min_tmp);
+        if (root->right) min_tmp=min(minDepth(root->right),min_tmp); 
+        
+
+        return 1+min_tmp;
+
     }
 };
