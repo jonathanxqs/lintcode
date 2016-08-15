a945c3b34a908ef600d54afd3821872f3d4969a1
Rhapsodistic <jonathanxqs@163.com>
2016-08-15 17:30:28 +0800

    Solved No.72

--- 72.cpp
+++ 72.cpp
@@ -21,5 +21,20 @@ class Solution {
 public:
     TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
         // write your code here
+    	int i,j,k,n=inorder.size(),m_mid,mid_val;
+    	if (n==0) return NULL;
+
+    	mid_val=postorder[n-1];
+    	m_mid=find(inorder.begin(),inorder.end(),mid_val)-inorder.begin();
+
+    	std::vector<int> in_l(inorder.begin(),inorder.begin()+m_mid),in_r(inorder.begin()+m_mid+1,inorder.end()),
+    	post_l(postorder.begin(),postorder.begin()+m_mid),post_r(postorder.begin()+m_mid,postorder.end()-1);
+
+    	
+        TreeNode *root=new TreeNode(mid_val);
+        root->left=buildTree(in_l,post_l);
+        root->right=buildTree(in_r,post_r);
+
+        return root;
     }
 };
