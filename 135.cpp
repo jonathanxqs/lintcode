1607f6500c6d17817a941bf1ea97c24d0985ae00
Rhapsodistic <jonathanxqs@163.com>
2016-08-22 01:03:40 -0400

    Solving 135

--- 135.cpp
+++ 135.cpp
@@ -7,5 +7,22 @@ public:
      */
     vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
         // write your code here
+        sort(candidates.begin(),candidates.end());
+        std::vector<int> v;
+        std::vector<std::vector<int> > rt_cs;
+
+        if (candidates[0]<=0) {
+        	cout<<"fault input,has non-positive number"<<endl;
+        	return rt_cs;
+        }
+
+        int rm_target=target,i=0,j,n,m;
+
+        while (1){
+        	rm_target -=
+
+        }
+
+
     }
 };
