230cfb4ee17e3269159c91e9d1faeb346ce4db77
Rhapsodistic <jonathanxqs@163.com>
2016-08-26 08:51:21 -0400

    Solved 135

--- 135.cpp
+++ 135.cpp
@@ -8,15 +8,18 @@ public:
 
     vector<int> candidates_ori;
     int n_cand;
+    std::vector<vector<int> > rt_cs;
+    int target;
 
     vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
         // write your code here
         sort(candidates.begin(),candidates.end());
         this->candidates_ori=candidates;
         this->n_cand=candidates.size();
+        this->target=target;
 
         std::vector<int> v;
-        std::vector<vector<int> > rt_cs;
+        
 
         if (n_cand==0) return rt_cs;
 
@@ -25,15 +28,37 @@ public:
         	return rt_cs;
         }
 
-        int rm_target=target,i=0,j,n,m;
+        depthFirstSearch(v,0,0);
 
-        if (candidates[0]==target) 
+        return rt_cs;
 
 
     }
 
-	depthFirstSearch(std::vector<int> &candidates,int target){
+	int depthFirstSearch(std::vector<int> &in_candidates,int i,int sum){
+		int j=0,k;
 
+		// cout<<i<<" j:"<<j<<" sum:"<<sum<<endl; 
+
+		if (sum==target){
+
+			if (find(rt_cs.begin(),rt_cs.end(),in_candidates) != rt_cs.end() ) return 2;
+			rt_cs.push_back(in_candidates);
+			return 1;
+		}
+
+		for (j=i;j<n_cand;j++){
+			if (sum+candidates_ori[j] <=target){
+				in_candidates.push_back(candidates_ori[j]);
+				depthFirstSearch(in_candidates,j,sum+candidates_ori[j]);
+				in_candidates.pop_back();
+
+			}
+			else break;
+		}
+
+		return 0;
 
 	}
+
 };
