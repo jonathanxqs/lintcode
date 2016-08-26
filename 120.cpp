7582bcea8a9cc26430983bc7c18879df945ba667
Rhapsodistic <jonathanxqs@163.com>
2016-08-26 12:49:12 -0400

    Start to solve 120

---
+++ 120.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    /**
+      * @param start, a string
+      * @param end, a string
+      * @param dict, a set of string
+      * @return an integer
+      */
+
+    int f[1111][1111];
+    int size_d;
+
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+        // write your code here
+        this->size_d=dict.size();
+
+    }
+};
