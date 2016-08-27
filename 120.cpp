3e6afb4093fa959694ee23937c1affc21f5bd8bb
Rhapsodistic <jonathanxqs@163.com>
2016-08-27 17:24:26 -0400

    Solved 120  by length simu

--- 120.cpp
+++ 120.cpp
@@ -6,55 +6,42 @@ public:
       * @param dict, a set of string
       * @return an integer
       */
-
-    int f[1111][1111];
-    int size_d;
-    const int NotLinkParaMax=INT_MAX/2-1;
-
-    int distancLize(string s1,string s2){
-    	if (s1.size()!=s2.size()) return 3;
-    	int count=0;
-    	for (int i=0;i<s1.size();i++){
-    		if (s1[i]!=s2[i]) count++; 
-    	}
-
-    	if (count==1) return 1;
-    	return 0;
-
-    }
-
     int ladderLength(string start, string end, unordered_set<string> &dict) {
-        // write your code here
-        this->size_d=dict.size();
-        std::vector<string> v_dict;
-        v_dict.append(start);
-
-        for (auto s:dict){
-        	v_dict.append(s);
+        if (start == end) {
+            return 1;
         }
-        v_dict.append(end);
-        memset(f,sizeof(f),0);
-        int i,j,k;
-        for (i=0;i<=size_d+1;i++){
-        	for (j=i+1;j<=size_d+1;j++)
-        		f[i][j]=f[j][i]=distancLize(v_dict[i],v_dict[j]);
+        int n = start.size();
+        if (n < 1 || n != end.size()) {
+            return 0;
         }
-
-        std::deque<int> shortDis_0(size_d+5,NotLinkParaMax);
-        for (i=0;i<=size_d+1;i++) shortDis_0[i]=f[0][i];
-        
-        std::set<int> inStack,notInStack;
-   	    for (i=0;i<=size_d+1;i++) notInStack.push_back()
-
-        	
-        for (i=0;i<=size_d+1;i++){
-        	for (j=0;j<=size_d+1;j++)
-        	{
-        		if ()
-        		f[i][j]=f[j][i]=distancLize(v_dict[i],v_dict[j]);
-        	}
+ 
+        queue<string> Q;
+        Q.push(start);
+        dict.erase(start);
+        int length = 2;
+ 
+        while (!Q.empty()) {
+            int size = Q.size();
+            for (int i = 0; i < size; i++) {
+                string word = Q.front(); Q.pop();
+                for (int i = 0; i < n; i++) {
+                    char oldChar = word[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == oldChar) continue;
+                        word[i] = c;
+                        if (word == end) {
+                            return length;
+                        }
+                        if (dict.find(word) != dict.end()) {
+                            Q.push(word);
+                            dict.erase(word);
+                        }
+                    }
+                    word[i] = oldChar;
+                }
+            } // for size
+            length++;
         }
-
-
+        return 0;
     }
 };
