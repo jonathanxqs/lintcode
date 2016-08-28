5eed1bc4c726054efe9f478f41e27fc1703c4d4d
Rhapsodistic <jonathanxqs@163.com>
2016-08-28 17:16:31 -0400

    Solving 137

---
+++ 137.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {
+public:
+    /**
+     * @param node: A undirected graph node
+     * @return: A undirected graph node
+     */
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        // write your code here
+    }
+};
--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Lintcode Problems solved by rhapsodistic
 
 No.139  O(n logn) yes O(n*n) no
 No.82  O(nlogn) sort  O(n)  xor   # 0 xor x=x   1 xor x=~x   x xor x=0
-No.99  python  one-pass   c++  extra vector
+No.99  python  one-pass   c++  extra vector  
+No.120 121   My WA 
