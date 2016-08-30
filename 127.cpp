5270c2637a18e31405c95b3958324f94502449d4
Rhapsodistic <jonathanxqs@163.com>
2016-08-29 22:51:56 -0400

    Solving 127

---
+++ 127.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for Directed graph.
+ * struct DirectedGraphNode {
+ *     int label;
+ *     vector<DirectedGraphNode *> neighbors;
+ *     DirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {
+public:
+    /**
+     * @param graph: A list of Directed graph node
+     * @return: Any topological order for the given graph.
+     */
+    vector<DirectedGraphNode*> topSort(vector<DirectedGraphNode*> graph) {
+        // write your code here
+        
+    }
+};
