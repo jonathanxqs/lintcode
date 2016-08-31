a7ddd441ed492a746b17c1df3afa71ba3f06b713
Rhapsodistic <jonathanxqs@163.com>
2016-08-31 12:55:09 -0400

    Solved 125

--- 125.cpp
+++ 125.cpp
@@ -11,21 +11,28 @@ public:
         int n=A.size();
         if (A.size()==0 or m==0) return 0;
 
-        int i,j,k,max=0;
-        std::vector<bool> dp_v(m+5,0);
-        dp_v[0]=1;
+        int i,j,k,max=0,sum_value=0;
+        for (auto const s:V) sum_value+=s;
+
+        std::vector<int> dp_v(sum_value+5,-1);
+        dp_v[0]=0;
 
         for (j=0;j<n;j++){
-        	for (i=m;i>=1;i--){        	
-        		if (i-A[j]>=0 and dp_v[i-A[j]]) {
-        			dp_v[i]=1;
+        	for (i=sum_value;i>=1;i--){        	
+        		if (i-V[j]>=0 and dp_v[i-V[j]]>-1) {
+        			if (dp_v[i]>-1 and dp_v[i-V[j]]+A[j]>dp_v[i]) continue;
+
+        			dp_v[i]=dp_v[i-V[j]]+A[j];
         			
         		}
-
-        	if (dp_v[i] and i>max) max=i;
         	}
         }
-        
+
+        for (i=sum_value;i>=0;i--)
+        	if (dp_v[i]>-1 and dp_v[i]<=m ) {
+        		max=i;
+        		return max;
+        	}        
 
         return max;
    
