57a01c8b9de0d4244221ad15f019f2b3c62bed72
Rhapsodistic <jonathanxqs@163.com>
2016-08-31 18:54:28 -0400

    Solved 564

--- 564.cpp
+++ 564.cpp
@@ -5,7 +5,30 @@ public:
      * @param target an integer
      * @return an integer
      */
+    std::vector<int> dp_ans;
+    
+    std::vector<int> v;
+
     int backPackVI(vector<int>& nums, int target) {
         // Write your code here
+        
+        sort(nums.begin(),nums.end());
+
+        if (nums.size()==0) return 0;
+
+        int n=nums.size(),i,j,k;
+        dp_ans.assign(target+5,0);
+        dp_ans[0]=1;
+
+        for (i=1;i<=target;i++){
+        	for (auto const s:nums){
+        		if (i-s<0) break;
+        		dp_ans[i] +=dp_ans[i-s];
+        	}
+        }
+
+
+        return dp_ans[target];
+
     }
 };
