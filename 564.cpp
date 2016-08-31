688b93fe845235aa499d6481665b54c1a57fe5c6
Rhapsodistic <jonathanxqs@163.com>
2016-08-31 12:56:37 -0400

    Solving 564

---
+++ 564.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /**
+     * @param nums an integer array and all positive numbers, no duplicates
+     * @param target an integer
+     * @return an integer
+     */
+    int backPackVI(vector<int>& nums, int target) {
+        // Write your code here
+    }
+};
