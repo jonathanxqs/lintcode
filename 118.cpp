3155933a1f1dfe6fe6901c1692f4030775b13355
Rhapsodistic <jonathanxqs@163.com>
2016-09-03 10:26:13 -0400

    Solving 118

---
+++ 118.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:    
+    /**
+     * @param S, T: Two string.
+     * @return: Count the number of distinct subsequences
+     */
+    int numDistinct(string &S, string &T) {
+        // write your code here
+    }
+};
