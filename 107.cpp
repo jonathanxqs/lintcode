f1dff244e6ae7bd580cbd343e5c0a4ae1dfaf82c
Rhapsodistic <jonathanxqs@163.com>
2016-09-02 22:59:28 -0400

    Solving 107

--- 107.cpp
+++ 107.cpp
@@ -13,17 +13,23 @@ public:
         dp_s.assign(l_s+5,0);
         dp_s[0]=true;
 
+        // cout<<l_s<<" "<<n<<endl; 
+
         for (i=1;i<=l_s;i++){
 
         	cout<<i<<":";
         	for (auto const di1:dict){
-        		if ( (i-di1.size())>=0 and dp_s[i-di1.size()] and di1==s.substr(i-di1.size(),di1.size()) )
-        		{
-        			dp_s[i]=true;
-        			break;
-        		}
-        		cout<<" "<<di1<<endl;;
+
+        		cout<<" "<<di1<<endl;
+        		if ((i-di1.size()) >=0) 
+        			if ( dp_s[i-di1.size()]==true and di1==s.substr(i-di1.size(),di1.size()) )
+	        		{
+	        			dp_s[i]=true;
+	        			break;
+	        		}
+
         	}
+
         }
 
         return dp_s[l_s];
