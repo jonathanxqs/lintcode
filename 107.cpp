517d6febdc2f37164dc5b5ad5ae204525dfcad8c
Rhapsodistic <jonathanxqs@163.com>
2016-09-03 10:25:03 -0400

    Solved 107

--- 107.cpp
+++ 107.cpp
@@ -7,26 +7,36 @@ public:
     std::vector<bool> dp_s;
     bool wordBreak(string s, unordered_set<string> &dict) {
         // write your code here
-        int l_s=s.size(),i,j,k,n=dict.size();
+        int l_s=s.size(),i=0,j,k,n=dict.size();
         if (n==0 and l_s>0) return false;
 
-        dp_s.assign(l_s+5,0);
+        dp_s.assign(l_s +10,false);
         dp_s[0]=true;
 
         // cout<<l_s<<" "<<n<<endl; 
 
         for (i=1;i<=l_s;i++){
 
-        	cout<<i<<":";
+        	 // cout<<i<<":";
+
         	for (auto const di1:dict){
 
-        		cout<<" "<<di1<<endl;
-        		if ((i-di1.size()) >=0) 
-        			if ( dp_s[i-di1.size()]==true and di1==s.substr(i-di1.size(),di1.size()) )
+        		 // cout<<" "<<di1<<di1.size()<<" "<<i-(int)di1.size()<<endl;
+
+        		if ((i-(int)di1.size()) >=0) 
+        		{
+        			// cout<<s.substr(i-(int)di1.size(),di1.size())<<endl;
+
+        			if ( dp_s[i-di1.size()]==true  
+        				and (di1 == s.substr(i-(int)di1.size(),di1.size())) )
 	        		{
 	        			dp_s[i]=true;
 	        			break;
 	        		}
+
+	        		
+        		} 
+
 
         	}
 
