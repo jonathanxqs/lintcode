1bd8bb17afe4995594507f815235e55eae169954
Rhapsodistic <jonathanxqs@163.com>
2016-09-04 11:41:37 -0400

    Solving 118

--- 118.cpp
+++ 118.cpp
@@ -5,21 +5,32 @@ public:
      * @return: Count the number of distinct subsequences
      */
 
-    int numDistinct(string &S, string &T) {
+     int numDistinct(string &S, string &T) {
         // write your code here
+        vector<vector<int>> t(T.length()+10,vector<int>(S.length()+10,0));
+        for(int i=0;i<=T.length();i++) t[i][0]=0;
+        for(int i=0;i<=S.length();i++) t[0][i]=1;// 1 or 0 ?
 
-        int len_s=S.size(),len_t=T.size();
-        int i,j,k,n;
-
-
-        std::vector<int> v(len_t+5,0);
-        std::vector<std::vector<int> > dp(len_s+5,v);
-
-        for (i=1;i<=len_s;i++){
-        	for (j=1;j<=len_t;j++){
-        		if (j>i) break;
-        		if (S[i]==T[j]) 
-        	}
+        for(int i=1;i<=S.length();i++)
+        {
+            if(T[0]==S[i-1])
+                t[1][i]=t[1][i-1]+1;
+            else
+                t[1][i]=t[1][i-1];    
         }
+        
+        
+        for(int i=2;i<=T.length();i++)
+        {
+            for(int j=1; j<=S.length();j++)
+            {
+                if(T[i-1]==S[j-1])
+                    t[i][j]=t[i-1][j-1]+t[i][j-1];
+                else
+                    t[i][j]=t[i][j-1];
+            }
+        }
+        return t[T.length()][S.length()];
     }
+
 };
