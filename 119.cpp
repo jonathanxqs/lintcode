0bff23c532d0d7040db2f2e1fee2dd4cec8b9b2c
Rhapsodistic <jonathanxqs@163.com>
2016-09-06 10:13:19 -0400

    Solving 119

--- 119.cpp
+++ 119.cpp
@@ -6,6 +6,7 @@ public:
      */
     int minDistance(string word1, string word2) {
         // write your code here
-        
+        std::vector<int> v(word2.size());
+        std::vector<std::vector<int> > v;
     }
 };
