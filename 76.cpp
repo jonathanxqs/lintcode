f6ec23b138dcff0e06f274114a203a86c6be08ba
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-11 21:59:30 -0400

    testing 76

--- 76.cpp
+++ 76.cpp
@@ -15,11 +15,11 @@ public:
         	for (j=0;j<i;j++)
         		if (nums[j]<nums[i]) dp[i]=max(dp[i],dp[j]+1);
 
-        	cout<<dp[i]<<"	";
+        	// cout<<dp[i]<<"	";
         }
 
         //find rt
-        for (i=1;i<n-1;i++){
+        for (i=0;i<n;i++){
         	rt_max=max(rt_max,dp[i]);
         }
 
