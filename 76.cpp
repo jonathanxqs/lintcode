5385d17a4f3d1b690b4c16b11251dc478f4ef906
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-11 21:49:29 -0400

    Solving 76

---
+++ 76.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    /**
+     * @param nums: The integer array
+     * @return: The length of LIS (longest increasing subsequence)
+     */
+    int longestIncreasingSubsequence(vector<int> nums) {
+        // write your code here
+    }
+};
--- 91.cpp
+++ 91.cpp
@@ -9,6 +9,7 @@ public:
         // write your code here
         int n = A.size();
         int f[n + 1][101];
+
         //init 
         for (int i = 0; i <= n ; ++i)
             for (int j = 0; j <=100; ++j)
@@ -16,7 +17,7 @@ public:
         for (int i = 0; i <= 100; ++i)
             f[0][i] = 0;
 
-
+        //dp
         for (int i = 1; i <=n; ++i)
             for (int  j = 0; j <= 100;++j)
                 if (f[i-1][j] != INT_MAX)
@@ -24,6 +25,7 @@ public:
                     if (abs(j-k) <= target)
                     if (f[i][k] > f[i-1][j] + abs(A[i-1]-k))
                         f[i][k] = f[i-1][j] + abs(A[i-1]-k);  
+
         int ans = INT_MAX;
         for (int i = 0; i <= 100; ++i)
             if (f[n][i] < ans)
