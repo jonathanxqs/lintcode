88a0a8731fc99c41906ec794f53446ccbb6a9111
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-12 15:26:34 -0400

    Another solution for 91

--- 91.cpp
+++ 91.cpp
@@ -34,3 +34,39 @@ public:
         return ans; 
     }
 };
+
+// another solution 
+// class Solution {
+//     const int MaxNum = 101;
+// public:
+//     /**
+//      * @param A: An integer array.
+//      * @param target: An integer.
+//      */
+//     int MinAdjustmentCost(vector<int> A, int target)
+//     {
+//         int n = A.size();
+//         vector<vector<int>> dp(n, vector<int>(MaxNum, INT_MAX));
+
+//         for(int i = 0; i < MaxNum; i ++)
+//             dp[0][i] =  abs(i - A[0]);
+
+//         for(int i = 1; i < n; i ++)
+//         {
+//             for(int k = 0; k < MaxNum; k ++)
+//             {
+//                 int cost = abs(k - A[i]);
+//                 for(int p = -target; p <= target; p ++)
+//                 {
+//                     int prev = k + p;
+//                     prev = min(prev, MaxNum - 1);
+//                     prev = max(prev, 0);
+                    
+//                     dp[i][k] = min(dp[i][k], cost + dp[i - 1][prev]);
+//                 }
+//             }
+//         }
+
+//         return *min_element(dp.back().begin(), dp.back().end());
+//     }
+// };
