4a5ff1681b339e254baf3b29d7bdaf7343135105
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-12 16:46:28 -0400

    Waiting for better alg

--- 4.cpp
+++ 4.cpp
@@ -1,55 +1,58 @@
-class Solution {
-public:
-    /*
-     * @param n an integer
-     * @return the nth prime number as description.
-     */
 
-    set<int> a;
 
-    bool isUgly(int i){
-    	int j=i;
-    	while (j % 2==0) j=j/2;
-    	while (j % 3==0) j=j/3;
-    	while (j % 5==0) j=j/5;
-    	if (j==1) return true;
-    	else return false;
-    }
+// TLE SOLUTION 
+// class Solution {
+// public:
+//     /*
+//      * @param n an integer
+//      * @return the nth prime number as description.
+//      */
 
-    int nthUglyNumber(int n) {
-        // write your code here
+//     set<int> a;
 
-        if (n<=0) return -1;
-        if (n==1) return 1;
+//     bool isUgly(int i){
+//     	int j=i;
+//     	while (j % 2==0) j=j/2;
+//     	while (j % 3==0) j=j/3;
+//     	while (j % 5==0) j=j/5;
+//     	if (j==1) return true;
+//     	else return false;
+//     }
+
+//     int nthUglyNumber(int n) {
+//         // write your code here
+
+//         if (n<=0) return -1;
+//         if (n==1) return 1;
+
+//         int i,j,k=1;
         
-        int i,j,k=1;
         
-        
-        i=1;
+//         i=1;
 
-        while (i<INT_MAX){
-        	i++;
-        	if (isUgly(i))
-        		{
-        			k++;
-        			if (k==n) return i;
-        		}
+//         while (i<INT_MAX){
+//         	i++;
+//         	if (isUgly(i))
+//         		{
+//         			k++;
+//         			if (k==n) return i;
+//         		}
 
-        }
+//         }
 
-        set<int>::iterator it1=a.begin();
-        for (int i=2;i<=n;i++) {
-        	  cout<<*it1<<"	";
-        	it1++;
-        }
+//         set<int>::iterator it1=a.begin();
+//         for (int i=2;i<=n;i++) {
+//         	  cout<<*it1<<"	";
+//         	it1++;
+//         }
 
-        return *it1;
-
+//         return *it1;
 
 
-    }
+
+//     }
 
     
 
 
-};
+// };
