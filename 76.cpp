0bf04a2ad1af934b1c563009c2342fbb1b1692ed
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-11 21:55:09 -0400

    Solving 76

--- 76.cpp
+++ 76.cpp
@@ -6,5 +6,20 @@ public:
      */
     int longestIncreasingSubsequence(vector<int> nums) {
         // write your code here
+        int n=nums.size();
+        int i,j,k,l,rt_max=0;
+        std::vector<int> dp(n+1,1);
+        for (i=1;i<n-1;i++){
+        	for (j=0;j<i-1;j++)
+        		if (nums[j]<nums[i]) dp[i]=max(dp[i],dp[j]+1);
+        }
+
+        for (i=1;i<n-1;i++){
+        	rt_max=max(rt_max,dp[i]);
+        }
+
+        return rt_max;
+
     }
+
 };
