01996a3cf4dc755efa00cc3949577bdf5c102a4d
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-14 19:32:52 -0400

    Solving 40

---
+++ 40.cpp
@@ -0,0 +1,21 @@
+class Queue {
+public:
+    stack<int> stack1;
+    stack<int> stack2;
+
+    Queue() {
+        // do intialization if necessary
+    }
+
+    void push(int element) {
+        // write your code here
+    }
+    
+    int pop() {
+        // write your code here
+    }
+
+    int top() {
+        // write your code here
+    }
+};
