10acba8db606f9b5531c799bc34184932193d48f
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-15 19:07:08 -0400

    Modify 151, change the alg

--- 151.cpp
+++ 151.cpp
@@ -5,36 +5,26 @@ public:
      * @return: Maximum profit
      */
     int maxProfit(vector<int> &prices) {
-        // write your code here
-        std::vector<int> lc_deltas;
-        
-        int rt_profit=0,i,j,k,n=prices.size();
-
-        i=0;
-        while (i<n){
-
-        	int tmp_profit=0;
-
-        	j=prices[i];
-        	while (i<n-1 and prices[i]<=prices[i+1]) i++;
-        	tmp_profit += prices[i]-j; 
-
-        	lc_deltas.push_back(tmp_profit);
-
-        	i++;
+        if (prices.size() == 0) {
+            return 0;
         }
-
-        sort(lc_deltas.begin(),lc_deltas.end());
-
-        int last=lc_deltas.size();
-        if (last-1 >=0 ) rt_profit +=lc_deltas[last-1];
-        if (last-2 >=0 ) rt_profit +=lc_deltas[last-2];
-
-        return rt_profit;
-
-
-
+        
+        vector<int> profit(prices.size());
+        int buy = 0;
+        buy = prices[0];
+        profit[0] = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            profit[i] = max(profit[i - 1], prices[i] - buy);
+            buy = min(buy, prices[i]);
+        }
+        
+        int sell = prices[prices.size() - 1];
+        int best = 0;
+        for (int i = prices.size() - 2; i >= 0; i--) {
+            best = max(best, sell - prices[i] + profit[i]);
+            sell = max(sell, prices[i]);
+        }
+        
+        return best;
     }
-
-
 };
---
+++ 393.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /**
+     * @param k: An integer
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    int maxProfit(int k, vector<int> &prices) {
+        // write your code here
+    }
+};
