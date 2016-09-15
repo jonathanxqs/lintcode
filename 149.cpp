aca414ad297b1a90cc327d33f8e0039b6ff6c277
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-15 14:56:15 -0400

    Solving 149

---
+++ 149.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    int maxProfit(vector<int> &prices) {
+        // write your code here
+        int min=INT_MAX,rt_profit=0;
+
+        for (auto const s:prices){
+        	if (s-min>rt_profit) rt_profit=s-min;
+        	if (s<min) min=s;
+
+        }
+
+        return rt_profit;
+        
+    }
+};
