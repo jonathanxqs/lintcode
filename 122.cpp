d5dd8d593e715bd43f4396f7dc0104b95f3ea1af
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-15 11:02:22 -0400

    Solving 122

--- 122.cpp
+++ 122.cpp
@@ -8,6 +8,9 @@ public:
 
     int largestRectangleArea(vector<int> &height) {
         // write your code here
+
+        //init 
+        int i,j,k,l=0;
         n=height.size();
         if (n<=0) {
         	sq_max=0;
@@ -16,7 +19,29 @@ public:
 
         for (auto const s:height) hei_sets.insert(s);
 
+        	//simulate
         for (auto const s:hei_sets){
+        	int i,tmp_leng=0,tmp_leng_max=0,sq_tmp=0;
+
+        	i=0;j=0;
+        	while (i<n){
+
+        		if (height[i]>=s){
+        			j++;
+        			tmp_leng_max=max(tmp_leng_max,j);
+
+        		} 
+        		else {
+        			tmp_leng_max=max(tmp_leng_max,j);
+        			j=0;
+        		}
+
+        		i++;
+
+        	}
+
+        	sq_tmp=tmp_leng_max*s;
+        	sq_max=max(sq_max,sq_tmp);
 
         }
 
@@ -28,7 +53,7 @@ public:
 private:
 	set<int> hei_sets;
 	int n;
-    int sq_max=INT_MAX;
+    int sq_max=0;
 
 
 };
