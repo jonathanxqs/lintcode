3667ead8d685cd8a09417eb14c93a4bef383ba17
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-16 22:44:48 -0400

    Find a solution

--- 393.cpp
+++ 393.cpp
@@ -6,6 +6,32 @@ public:
      * @return: Maximum profit
      */
     int maxProfit(int k, vector<int> &prices) {
-        // write your code here
+        if (prices.empty())
+            return 0;
+
+        int ans = 0;
+        if (k >= prices.size() / 2)
+        {
+            for (int i = 1; i < prices.size(); ++i) {
+                if (prices[i] - prices[i - 1] > 0) {
+                    ans += prices[i] - prices[i - 1];
+                }
+            }
+        } else {
+            vector<int> local(k+1);
+            vector<int> global(k+1);
+            
+            for (int i = 0; i < prices.size() - 1; ++i) {
+                int increase = prices[i + 1] - prices[i];
+    
+                for (int j = k; j >= 1; --j) {
+                    local[j] = max(global[j - 1] + max(increase, 0), local[j] + increase);
+                    global[j] = max(global[j], local[j]);
+                }
+            }
+            ans = global[k];
+        }
+        return ans;
     }
+   
 };
