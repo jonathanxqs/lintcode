2a81447d6b2d1da687c2067b991fe454d6bdf42d
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-16 23:54:24 -0400

    Solving 123

--- 123.cpp
+++ 123.cpp
@@ -5,7 +5,22 @@ public:
      * @param word: A string
      * @return: A boolean
      */
+    const int dx[4]={1,0,-1,0};
+    int dy[4]={0,1,0,-1} const;
+    bool dfs(int i,int j,std::vector<int> &vx,vector<int> &vy){
+    	if ()
+
+    }
     bool exist(vector<vector<char> > &board, string word) {
         // write your code here
+        row=board.size();
+        if (row<=0) return false;
+        col=board[0].size();
+        if (col<=0) return false;
+
+        //find the start point 
     }
+
+private:
+	int row,col;
 };
