d4894ee0dd08b976131f931f61e45a55f8770a11
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-16 23:45:57 -0400

    Solving 123

---
+++ 123.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /**
+     * @param board: A list of lists of character
+     * @param word: A string
+     * @return: A boolean
+     */
+    bool exist(vector<vector<char> > &board, string word) {
+        // write your code here
+    }
+};
