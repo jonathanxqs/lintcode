885129086ce6eb156b49a4b4a1593d1aef43bb44
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-17 23:35:54 -0400

    testing 3

--- 3.cpp
+++ 3.cpp
@@ -7,5 +7,20 @@ public:
      */
     int digitCounts(int k, int n) {
         // write your code here
+        if (k<0 or k>10) return 0;
+        int rt_count=0;
+        int i,j,ys=0,n_it=n;
+
+        i=1;
+        while (n_it>0){
+        	ys=n_it%10;
+
+        	n_it=n_it/10;
+        	rt_count+=n_it;
+        	if (ys>=k) rt_count+=1;
+        }
+
+        return rt_count;
+
     }
 };
