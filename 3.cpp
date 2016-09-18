28ace314fb423f49658c8d8ad7900d3d2728634a
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-17 23:27:11 -0400

    Solving 3

---
+++ 3.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /*
+     * param k : As description.
+     * param n : As description.
+     * return: How many k's between 0 and n.
+     */
+    int digitCounts(int k, int n) {
+        // write your code here
+    }
+};
