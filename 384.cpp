db181d3eed1bc3ca8bd01e522f516eb270846f05
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-21 23:59:09 -0400

    Fixed the bug

--- 384.cpp
+++ 384.cpp
@@ -11,14 +11,17 @@ public:
         int max_len=0;
         for (char c='a';c<='z';c++)
         	mapCtoSub[c]=-1;
-        cout<<s<<l_s<<endl;
-        for (i=0,j=0;j++;j<l_s){
+
+        // cout<<s<<l_s<<endl;
+
+        for (i=0,j=0;j<l_s;j++){
+        	// cout<<i<<" "<<j<<endl;
         	if (mapCtoSub[s[j]]>-1) i=max(i,mapCtoSub[s[j]]);
         	mapCtoSub[s[j]]=j;
         	int tmp_len=j-i;
         	if (tmp_len>max_len) max_len=tmp_len;
 
-        	cout<<i<<" "<<j<<endl;
+        	// cout<<i<<" "<<j<<endl;
 
         }
 
