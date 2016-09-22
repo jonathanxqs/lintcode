8623bfbdaf3af1a40eeb7494ee67a853eb9b8cda
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-21 23:50:12 -0400

    solving 384

--- 384.cpp
+++ 384.cpp
@@ -7,7 +7,16 @@ public:
     std::map<char, int> mapCtoSub;
     int lengthOfLongestSubstring(string s) {
         // write your code here
-        int i,j,k;
+        int i,j,k,l_s=s.size();
+        int max_len=0;
+        for (char c='a';c<='z';c++)
+        	mapCtoSub[c]=-1;
+
+        for (i=0,j=0;j++;j<=){
+        	if (mapCtoSub[s[j]]>-1) i=max(i,mapCtoSub[s[j]]);
+        	int tmp_len=j-i;
+
+        }
 
 
     }
