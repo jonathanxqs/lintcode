30271e4a9e0c04012d56e6a58c6f6606b4ca453f
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-21 23:20:36 -0400

    Solving 384

---
+++ 384.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    /**
+     * @param s: a string
+     * @return: an integer 
+     */
+    std::map<char, int> mapCtoSub;
+    int lengthOfLongestSubstring(string s) {
+        // write your code here
+        int i,j,k;
+
+
+    }
+};
