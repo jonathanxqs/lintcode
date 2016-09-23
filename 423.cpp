e29bee32c1b57589a205c138e069a65a524e924e
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-22 23:15:13 -0400

    Solving 423

---
+++ 423.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    /**
+     * @param s A string
+     * @return whether the string is a valid parentheses
+     */
+    bool isValidParentheses(string& s) {
+        // Write your code here
+    }
+};
