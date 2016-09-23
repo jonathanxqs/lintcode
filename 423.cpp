acdbcecd564fccf74051b8bf98888f0a9733907f
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-22 23:31:02 -0400

    solving 423

--- 423.cpp
+++ 423.cpp
@@ -4,7 +4,24 @@ public:
      * @param s A string
      * @return whether the string is a valid parentheses
      */
+
+    deque<int> que1;
+    map<char,int> baseOfPar;
     bool isValidParentheses(string& s) {
         // Write your code here
+
+        //init 
+        int i,j,k,l_s=s.size();
+        string s_tem=s;
+
+        baseOfPar['(']=1;        baseOfPar[')']=4;
+        baseOfPar['[']=2;        baseOfPar[']']=5;
+        baseOfPar['{']=3;        baseOfPar['}']=6;
+
+        //loop
+        for (i=0;i<l_s;i++){
+        	if 
+        }
     }
+
 };
