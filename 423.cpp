d421fcdf11407e242d2be3c5f7290985d7c83d1d
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-22 23:59:26 -0400

    9zhang

--- 423.cpp
+++ 423.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    /**
+
+	/**
      * @param s A string
      * @return whether the string is a valid parentheses
      */
-
-    deque<int> que1;
-    map<char,int> baseOfPar;
-    bool isValidParentheses(string& s) {
-        // Write your code here
-
-        //init 
-        int i,j,k,l_s=s.size();
-        string s_tem=s;
-
-        baseOfPar['(']=1;        baseOfPar[')']=4;
-        baseOfPar['[']=2;        baseOfPar[']']=5;
-        baseOfPar['{']=3;        baseOfPar['}']=6;
-
-        //loop
-        for (i=0;i<l_s;i++){
-        	if (baseOfPar[s[i]]<=3) que1.
-
-        	else {
-        		s
-        	}
+    /*
+    题意：输入一个只包含括号的字符串，判断括号是否匹配
+    模拟堆栈，读到左括号压栈，读到右括号判断栈顶括号是否匹配
+    */
+    bool isValidParentheses((string s) {
+        int len = s.length();
+        vector<char> stack;
+        for (int i = 0; i < len; i++) {
+            // 左括号压栈
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') stack.push_back(s[i]);
+            else {
+                // 右括号出栈
+                if (stack.empty()) return false;
+                char top = stack[stack.size() - 1];
+                if (s[i] == ')' && top != '(')
+                    return false;
+                if (s[i] == ']' && top != '[') 
+                    return false;
+                if (s[i] == '}' && top != '{') 
+                    return false;
+                stack.pop_back();
+            }
         }
+        // 栈中无多余左括号
+        if (stack.size() > 0) return false;
+        return true;
     }
-
 };
