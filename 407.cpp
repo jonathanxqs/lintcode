37d8424bad8c4cb1b333d59c5168123969119f3d
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-23 23:15:18 -0400

    test 407

--- 407.cpp
+++ 407.cpp
@@ -10,11 +10,12 @@ public:
         int tmp,s_tmp,jw=0;
         std::vector<int> vRev(digits.rbegin(),digits.rend());
         for (auto  s:vRev){
+        	cout<<s<<" ";
         	s_tmp=jw+s+1;
         	s=s_tmp%10;
         	jw=s_tmp/10;        	
         }
-        if (jw>1) v.push_back(jw);
+        if (jw>1) vRev.push_back(jw);
 
         std::vector<int> rt_v(vRev.rbegin(),vRev.rend());
         return rt_v;
