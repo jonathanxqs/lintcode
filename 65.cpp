2afd99db7fe7499b24fa74074936880d3a5ace60
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-24 18:11:28 -0400

    Solved problem

---
+++ 65.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /**
+     * @param A: An integer array.
+     * @param B: An integer array.
+     * @return: a double whose format is *.5 or *.0
+     */
+    double findMedianSortedArrays(vector<int> A, vector<int> B) {
+        // write your code here
+    }
+};
