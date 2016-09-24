1addbe2778166556d74d393ad3f5dfa3e35bee64
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-23 23:09:20 -0400

    Solving 417

---
+++ 407.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    /**
+     * @param digits a number represented as an array of digits
+     * @return the result
+     */
+    vector<int> plusOne(vector<int>& digits) {
+        // Write your code here
+        int i,j,k,n=digits.size();
+        
+    }
+};
