b7b90b6ba25f0c35272531de7715171a48fdeb88
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-23 23:13:23 -0400

    Solving 407

--- 407.cpp
+++ 407.cpp
@@ -7,6 +7,16 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         // Write your code here
         int i,j,k,n=digits.size();
-        
+        int tmp,s_tmp,jw=0;
+        std::vector<int> vRev(digits.rbegin(),digits.rend());
+        for (auto const s:vRev){
+        	s_tmp=jw+s+1;
+        	s=s_tmp%10;
+        	jw=s_tmp/10;        	
+        }
+        if (jw>1) v.push_back(jw);
+
+        std::vector<int> rt_v(vRev.rbegin(),vRev.rend());
+        return rt_v;
     }
 };
