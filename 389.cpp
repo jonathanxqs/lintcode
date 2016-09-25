fa4ccf6f7ed459833fbc3d98b4f91215473024ec
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-24 23:57:18 -0400

    Solving 389

---
+++ 389.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    /**
+      * @param board: the board
+      * @return: wether the Sudoku is valid
+      */
+    bool isValidSudoku(const vector<vector<char>>& board) {
+        
+    }
+};
