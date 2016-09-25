d52576dee016ca061c2a133d93d0b6a67035cc26
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-25 17:22:29 -0400

    Consider situation of n<0

--- 428.cpp
+++ 428.cpp
@@ -11,9 +11,14 @@ public:
         if (x==0) return 0;
         int i,j;
 
-        for (i=0;i<n;i++){
+        int abs_n=abs(n);
+        
+
+        for (i=0;i<abs_n;i++){
         	rt_x *= x;
         }
+
+        if (n<0) rt_x=1/rt_x;
 
         return rt_x;
     }
