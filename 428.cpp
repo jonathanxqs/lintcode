59e41b32cf56ca5100b02ad14908eb26ff9d9b31
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-25 17:18:25 -0400

    Solving 428

---
+++ 428.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /**
+     * @param x the base number
+     * @param n the power number
+     * @return the result
+     */
+    double myPow(double x, int n) {
+        // Write your code here
+    }
+};
