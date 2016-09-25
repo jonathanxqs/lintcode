6095a1c1f8b770140b2c5fdf96b0c99c5b72e3b7
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-25 17:19:59 -0400

    testing 428

--- 428.cpp
+++ 428.cpp
@@ -7,5 +7,14 @@ public:
      */
     double myPow(double x, int n) {
         // Write your code here
+        double rt_x=1;
+        if (x==0) return 0;
+        int i,j;
+
+        for (i=0;i<n;i++){
+        	rt_x *= rt_x;
+        }
+
+        return rt_x;
     }
 };
