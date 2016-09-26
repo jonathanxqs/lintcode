f155ff607528a586fa456410737ef72850e28c0b
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-26 15:44:57 -0400

    Solving 420

--- 420.cpp
+++ 420.cpp
@@ -4,7 +4,37 @@ public:
      * @param n the nth
      * @return the nth sequence
      */
+    std::vector<string> countAndSaySeqs;
+    typedef pair<int,char> PAIR_TIMES_CHAR;
     string countAndSay(int n) {
         // Write your code here
+        string s1(""),s2;
+        countAndSaySeqs.push_back(s1);
+        s1="1";
+        countAndSaySeqs.push_back(s1);
+        if (n<=1) return countAndSaySeqs[n];
+
+        int i,j,k,m;
+        i=2;
+        while (i<=n){
+        	countAndSaySeqs.push_back(genNext(countAndSaySeqs[i-1]));
+        	i++;
+        }
+        return countAndSaySeqs[n];
+    }
+
+    string genNext(string s_prev){
+    	//pair  base times ->char
+    	pair<int,char> p1,p2;
+    	int i,j,k;
+    	string rt_next;
+    	char c_prev='&';// impossible char
+    	if (s_prev.size()<1) return rt_next;
+
+    	for (auto const s:s_prev){
+
+    	}
+
+    	return rt_next;
     }
 };
