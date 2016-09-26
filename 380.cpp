5ba8f5344bebc087bcf109647f1da942ff67b59d
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-25 20:17:30 -0400

    Find the best solution for 380

--- 380.cpp
+++ 380.cpp
@@ -13,9 +13,28 @@ public:
      * @param headB: the second list
      * @return: a ListNode
      */
+    map<ListNode *,int> nodeAlready;
+    
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        // write your code here
+        ListNode *pa=headA,*pb=headB;
+        int lengthA=0,lengthB=0;
+        while(pa) {pa=pa->next;lengthA++;}
+        while(pb) {pb=pb->next;lengthB++;}
         
-        
-    }
+        if(lengthA<=lengthB){
+          int n=lengthB-lengthA;
+          pa=headA;pb=headB;
+          while(n) {pb=pb->next;n--;}
+        }else{
+          int n=lengthA-lengthB;
+          pa=headA;pb=headB;
+          while(n) {pa=pa->next;n--;}
+        }
+        while(pa!=pb){
+          pa=pa->next;
+          pb=pb->next;
+        }
+        return pa;
+      }
+
 };
