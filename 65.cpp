e84636fdb842b7203d06f7639d25db2e740a4658
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-28 14:16:03 -0400

    Read the best solution O(log n+m)

--- 65.cpp
+++ 65.cpp
@@ -1,15 +1,15 @@
-class Solution {
+/*class Solution {
 public:
-    /**
+    *
      * @param A: An integer array.
      * @param B: An integer array.
      * @return: a double whose format is *.5 or *.0
-     */
+     
     double findMedianSortedArrays(vector<int> A, vector<int> B) {
         // write your code here
         // 
     }
-};
+};*/
 
 
 
