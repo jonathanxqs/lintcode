e1052b5f43a9a2d2c140da7646c55a3cf6d0cda1
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-28 11:16:23 -0400

    using trie to solve 132

--- 132.cpp
+++ 132.cpp
@@ -14,6 +14,7 @@ public:
     
     vector<string> results;
     Node * root;
+    //trie insert 
     void insert(Node* p, string s) {
         int len = s.size();
         for (int i = 0; i < len; ++i) {
