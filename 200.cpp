3bbc4ebc4aa827798b98fee55f0e16f11a82b757
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-29 00:10:00 -0400

    Solving 200

--- 200.cpp
+++ 200.cpp
@@ -9,7 +9,22 @@ public:
         int i,j,k,lenS = s.size();
         int tmp_len;
         string rtLongPalin;
+        int last_len = -1 , left_b = 0 ,right_b = 0 , max_len = 0;
+
         for (i=0;i<lenS;i++){
+            if (i-last_len-1>=0)
+                if ( s[i] == s[i-last_len-1]){
+                    last_len += 2;
+                }
+                else {
+                    last_len = -1;
+                }
+
+            if (last_len > max_len){
+                max_len = last_len;
+                right_b=i;
+                left_b=i-last_len-1;
+            }
 
         }
     }
