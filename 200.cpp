a3ef9b74344a5c8ac05d7e2c01d6cf22930e8455
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-28 23:54:08 -0400

    Solving 200

---
+++ 200.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    /**
+     * @param s input string
+     * @return the longest palindromic substring
+     */
+    string longestPalindrome(string& s) {
+        // Write your code here
+    }
+};
