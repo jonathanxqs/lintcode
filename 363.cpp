f105e94c79c2070bdefda4bb16d226944a8d0973
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-30 12:22:02 -0400

    Solving 363

---
+++ 363.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /**
+     * @param heights: a vector of integers
+     * @return: a integer
+     */
+    int trapRainWater(vector<int> &heights) {
+        // write your code here
+        // 
+    }
+};
