65c6157aec09a658e48e0a7c6b0fde6821bd9e19
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-30 12:34:27 -0400

    Solving 363

--- 363.cpp
+++ 363.cpp
@@ -6,6 +6,21 @@ public:
      */
     int trapRainWater(vector<int> &heights) {
         // write your code here
-        // 
+        // init
+        int len=heights.size();
+        if (len<=1) return 0;
+        std::vector<int> peaksSubscript,sumTill;
+        int peak=heights[0],sum=0;
+        int i,j,k;
+        sumTill.push_back(0);
+        for (auto const s:heights){
+            sum+=s;
+            sumTill.push_back(sum);
+        }
+
+        //find the peak array
+        for (i=1;i<len-1;i++){
+            if (hei)
+        }
     }
 };
