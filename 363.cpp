fb1be562908a4bc6a089a2f68a515e2dd44e63ee
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-30 15:53:34 -0400

    Solving 363

--- 363.cpp
+++ 363.cpp
@@ -9,18 +9,24 @@ public:
         // init
         int len=heights.size();
         if (len<=1) return 0;
-        std::vector<int> peaksSubscript,sumTill;
-        int peak=heights[0],sum=0;
+        //add 0 and 0 to head and tail
+        std::vector<int> heightsHeadTail(heights);
+        heightsHeadTail.insert(heightsHeadTail.begin(),0);
+        heightsHeadTail.insert(heightsHeadTail.end(),0);
+        std::vector<int> peaksSubscript(len+3,0),tillRight(len+3,0),tillLeft(len+3,0);
+        int rt_sum=0;
         int i,j,k;
-        sumTill.push_back(0);
+        
         for (auto const s:heights){
             sum+=s;
             sumTill.push_back(sum);
         }
 
         //find the peak array
+        if (heights[0]>heights[1])  
         for (i=1;i<len-1;i++){
-            if (hei)
+            if (heights[i]>=heights[i-1] and heights[i]<=heights[i+1])
+                peaksSubscript.push_back(i);
         }
     }
 };
