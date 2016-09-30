843a9f4f224dcf48a1f110ba81de4316f3860b6f
Rhapsodistic <xucanxucan123@gmail.com>
2016-09-30 16:32:21 -0400

    Find O(n) solution

--- 363.cpp
+++ 363.cpp
@@ -13,19 +13,22 @@ public:
         std::vector<int> heightsHeadTail(heights);
         heightsHeadTail.insert(heightsHeadTail.begin(),0);
         heightsHeadTail.insert(heightsHeadTail.end(),0);
-        std::vector<int> peaksSubscript(len+3,0),tillRight(len+3,0),tillLeft(len+3,0);
-        int rt_sum=0;
+        std::vector<int> peaksSubscript(len+5,0);
+
+        int left_b=0,right_b=0;
+        int rt_sum=0 , sum=0;
         int i,j,k;
         
-        for (auto const s:heights){
+        for (auto const s:heightsHeadTail){
             sum+=s;
             sumTill.push_back(sum);
         }
 
         //find the peak array
-        if (heights[0]>heights[1])  
+        
+        sum = 0;
         for (i=1;i<len-1;i++){
-            if (heights[i]>=heights[i-1] and heights[i]<=heights[i+1])
+            if (heightsHeadTail[i] >= heightsHeadTail[left_b]  )
                 peaksSubscript.push_back(i);
         }
     }
