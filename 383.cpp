002e7803d5917ecb38d6bfe5fdacad260b020fc0
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-02 23:42:33 -0400

    9zhang solution

--- 383.cpp
+++ 383.cpp
@@ -8,3 +8,24 @@ public:
         // write your code here
     }
 };
+
+class Solution {
+public:
+    int maxArea(vector<int> &height) {
+        int max = 0, area;
+        int start = 0, end = height.size() - 1;
+        while (start < end) {
+            if (height[start] > height[end]) {
+                area = height[end] * (end - start);
+                end --;
+            } else {
+                area = height[start] * (end - start);
+                start ++;
+            }
+            if (area > max) {
+                max = area;
+            }
+        }
+        return max;
+    }
+};
