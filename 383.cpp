2f70493f5944bd6fe884f0cfc0968309027b5387
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-02 23:57:55 -0400

    Considering 393

--- 383.cpp
+++ 383.cpp
@@ -1,5 +1,4 @@
 
-
 class Solution {
 public:
     /**
@@ -9,6 +8,8 @@ public:
     int maxArea(vector<int> &height) {
         int max = 0, area;
         int start = 0, end = height.size() - 1;
+
+        // sub
         while (start < end) {
             if (height[start] > height[end]) {
                 area = height[end] * (end - start);
@@ -17,10 +18,12 @@ public:
                 area = height[start] * (end - start);
                 start ++;
             }
+            
             if (area > max) {
                 max = area;
             }
         }
+
         return max;
     }
 };
