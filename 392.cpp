f3d36c56eada2c214ffd6458d38e1d74882bd52d
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-02 23:45:43 -0400

    Solving 392

---
+++ 392.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    /**
+     * @param A: An array of non-negative integers.
+     * return: The maximum amount of money you can rob tonight
+     */
+    long long houseRobber(vector<int> A) {
+        // write your code here
+    }
+};
