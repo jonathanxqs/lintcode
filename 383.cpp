cff8e442834c55aaccf01fde3851c4e7ebfd7640
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-02 22:45:18 -0400

    Solving 383

---
+++ 383.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    /**
+     * @param heights: a vector of integers
+     * @return: an integer
+     */
+    int maxArea(vector<int> &heights) {
+        // write your code here
+    }
+};
