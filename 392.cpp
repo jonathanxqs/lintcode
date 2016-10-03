905e2c2ddc538cd304ba1ca874c79543ae45b053
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-02 23:49:51 -0400

    testing 392

--- 392.cpp
+++ 392.cpp
@@ -6,5 +6,12 @@ public:
      */
     long long houseRobber(vector<int> A) {
         // write your code here
+        int i,j,k,n = A.size();
+
+        std::vector<long long> totTilN(n+5,0);  // 0->0 1->0 2->the first ,sub0
+        for (i=2;i<2+n;i++)
+            totTilN[i]=max(totTilN[i-2]+A[i-2],totTilN[i-1]);
+
+        return totTilN[n+1];
     }
 };
