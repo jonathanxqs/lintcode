1764b9639734630f911702756f254b1796118178
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-13 12:41:46 -0400

    Solving 415

---
+++ 415.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    /**
+     * @param s A string
+     * @return Whether the string is a valid palindrome
+     */
+    bool isPalindrome(string& s) {
+        // Write your code here
+        int len_s  = s.size(),i,j,k;
+        
+
+
+        string s1;
+        for (auto ch1:s){
+            if (::isalpha(ch1)) 
+                s1 += ::tolower(ch1);
+        }
+
+        // cout<<
+        int len_s1 = s1.size();
+        if (len_s1 == 0) return true;
+
+        for (i=0;i<len_s1;i<=len_s1-i-1)
+            if (s1[i] != s1[len_s1-1-i])
+                return false;
+
+        return true;
+
+    }
+};
