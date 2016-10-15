f985f5dbea064c594f59729e64e6222137b1d655
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-14 23:50:07 -0400

    Solving 488

---
+++ 488.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    /**
+     * @param n an integer
+     * @return true if this is a happy number or false
+     */
+    bool isHappy(int n) {
+        // Write your code here
+    }
+};
