b4a66fecdedf04953922866952672e08fae2ac57
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-15 10:29:04 -0400

    Thinking about a solutiom

--- 488.cpp
+++ 488.cpp
@@ -4,7 +4,12 @@ public:
      * @param n an integer
      * @return true if this is a happy number or false
      */
+    
+    stringstream s;
     bool isHappy(int n) {
         // Write your code here
+        s<<n;
+
+        int tmp;
     }
 };
