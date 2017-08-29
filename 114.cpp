a1fd24564b9a31fa813c95dad2cb38f70fba5639
Rhapsodistic <xucanxucan123@gmail.com>
2017-08-29 18:10:52 -0400

    114

--- 114.cpp
+++ 114.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    /**
-     * @param n, m: positive integer (1 <= n ,m <= 100)
-     * @return an integer
+     /*
+     * @param m: positive integer (1 <= m <= 100)
+     * @param n: positive integer (1 <= n <= 100)
+     * @return: An integer
      */
     int uniquePaths(int m, int n) {
         if (m < 1 || n < 1) {
             return 0;
         }
 
-        vector<vector<int> > ret(m, vector<int>(n, 1));
+        vector<vector<int> > ret(m, vector<int>(n, 1));  // m*n  1
 
         for (int i = 1; i != m; ++i) {
             for (int j = 1; j != n; ++j) {
