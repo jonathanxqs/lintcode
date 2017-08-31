59a111d68a243e4f7e8b52910187b7c183fb6dc3
Rhapsodistic <xucanxucan123@gmail.com>
2017-08-31 11:42:48 -0400

    366 commit

---
+++ 366.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    /*
+     * @param n: an integer
+     * @return: an ineger f(n)
+     */
+    // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+    // 1 -> 0
+    
+    int fibonacci(int n) {
+        // write your code here
+        int m=1,i=0,j=1,q=0;
+
+        if (n== 1 || n < 1) return 0;
+        while (m <n){
+            m++;
+            q = i+j;
+            i = j;
+            j = q;
+        }
+        return j;
+
+    }
+};
