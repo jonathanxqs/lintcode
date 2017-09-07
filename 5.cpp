22f82e66c2554df0dc8df23f4511c0d8b1c50e41
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-06 22:50:21 -0400

    go 5

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    /*
+     * param k : description of k
+     * param nums : description of array and index 0 ~ n-1
+     * return: description of return
+     */
+    int kthLargestElement(int k, vector<int> nums) {
+        // write your code here
+        // 
+        
+    }
+};
