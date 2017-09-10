b64cfc164ff379d849efe5ccd6cf99c7834ce13e
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-09 23:24:27 -0400

    test 388

--- 388.cpp
+++ 388.cpp
@@ -7,5 +7,42 @@ public:
      */
     string getPermutation(int n, int k) {
         // write your code here
-    }
+        // 
+        
+
+        int i,j,sum = 1;
+
+        //sum = (n-1)!
+        for (i=2; i<n; i++) {
+            sum *= i;
+        }
+        bool visited[n+2];
+        memset(visited, false, sizeof(visited));
+        string str;
+        for(i=1;i<n;i++){
+            int nextidx = k/sum;
+            k = k%sum;
+            if(k==0)
+                nextidx -- ;
+            sum/=(n-i);
+            int cnt = 0;
+            for (j=0; j<n; j++) {
+                if (!visited[j]) {
+                    if (cnt == nextidx){
+                        visited[j] = true;
+                        str += '0' +j+1;
+                        break;
+                    }
+                    cnt ++;
+                }
+            }
+        }
+        for(j=n-1;j>=0;j--){
+            if (!visited[j]) {
+                str += '0' + j+1;
+            }
+        }
+        return str;
+
+    };
 };
